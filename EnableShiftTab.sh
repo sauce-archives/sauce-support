50fa4ad847cdc855d75a695f564fcd1a6d01e459
Matthew Freeman <matt@saucelabs.com>
2014-01-21 12:32:37 -0600

    Add script to enable Safari tab key to highlight webpage items

---
+++ EnableShiftTab.sh
@@ -0,0 +1,4 @@
+
+#!/bin/sh 
+# This script enables Safari to use tab to higlight each item on a webpage
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2TabsToLinks -bool YES
